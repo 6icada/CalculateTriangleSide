3f611d74c243b07656c5ed5c09d43e501ee4673f
6icada <84038008+6icada@users.noreply.github.com>
2022-02-22 13:06:55 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+Code by 6icada
+Please do not copy code
+
+Input: Two sides of triangle
+Output: Third side of triangle (All possible values)
---
+++ main.py
@@ -0,0 +1,35 @@
+# Code by 6icada
+# Please do not copy code
+
+# Calculate function
+def Calculate(firstSide, secondSide):
+    if int(firstSide) - int(secondSide) <= 0:
+        # Adding vars
+        intLowest = int(secondSide) - int(firstSide) + 1
+        intHighest = int(firstSide) + int(secondSide) - 1
+        stringLowest = str(intLowest)
+        stringHighest = str(intHighest)
+
+        # Output
+        if intLowest == intHighest:
+            print(f'-- : {intLowest} : --\n')
+        else:
+            print(f'{stringLowest} : {stringHighest}\n')
+    else:
+        # Adding vars
+        intLowest = int(firstSide) - int(secondSide) + 1
+        intHighest = int(firstSide) + int(secondSide) - 1
+        stringLowest = str(intLowest)
+        stringHighest = str(intHightest)
+
+        # Output
+        if intLowest == intHighest:
+            print(f'-- : {intLowest} : --\n')
+        else:
+            print(f'{stringLowest} : {stringHighest}\n')
+
+# Adding vars
+firstSide = input('Enter first side: ')
+secondSide = input('Enter second side: ')
+
+Calculate(firstSide, secondSide)
